d5660d3eb61796e6910b4e10705a7d8fcb91aed2
Biloliddin <bilolidding@gmail.com>
2026-08-01 14:46:46 +0500

    Fix: ip_address migration added

--- messe/admin.py
+++ messe/admin.py
@@ -1,3 +1,8 @@
 from django.contrib import admin
+from .models import Message
 
-# Register your models here.
+# Message modelini admin panelga qo'shish
+@admin.register(Message)
+class MessageAdmin(admin.ModelAdmin):
+    list_display = ('username', 'body', 'ip_address', 'created') # Admin panelda ko'rinadigan ustunlar
+    search_fields = ('username', 'body', 'ip_address')            # Qidirish uchun maydonlar

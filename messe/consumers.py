b58c8f3217fb249c265b6148dc4f3e1792b9b2cb
Biloliddin <bilolidding@gmail.com>
2026-08-01 14:38:20 +0500

    IP manzil va models yangilandi

--- messe/consumers.py
+++ messe/consumers.py
@@ -1,58 +1,60 @@
 import json
-from channels.generic.websocket import AsyncWebsocketConsumer
 from channels.db import database_sync_to_async
+from channels.generic.websocket import AsyncWebsocketConsumer
 from .models import Message
+
 
 class ChatConsumer(AsyncWebsocketConsumer):
 
-    async def connect(self):
-        self.room_name = 'global_chat'
-        self.room_group_name = 'chat_%s' % self.room_name
+  async def connect(self):
+    self.room_name = 'global_chat'
+    self.room_group_name = 'chat_%s' % self.room_name
 
-        # Guruhga qo'shilish
-        await self.channel_layer.group_add(
-            self.room_group_name,
-            self.channel_name
-        )
-        await self.accept()
+    # Foydalanuvchining IP manzilini olish
+    headers = dict(self.scope['headers'])
+    if b'x-forwarded-for' in headers:
+      self.client_ip = headers[b'x-forwarded-for'].decode('utf-8').split(',')[0]
+    else:
+      self.client_ip = self.scope['client'][0]
 
-    async def disconnect(self, close_code):
-        # Guruhdan chiqish
-        await self.channel_layer.group_discard(
-            self.room_group_name,
-            self.channel_name
-        )
+    await self.channel_layer.group_add(
+        self.room_group_name, self.channel_name
+    )
+    await self.accept()
 
-    async def receive(self, text_data):
-        data = json.loads(text_data)
-        message = data['message']
-        username = data['username']
+  async def disconnect(self, close_code):
+    await self.channel_layer.group_discard(
+        self.room_group_name, self.channel_name
+    )
 
-        # Xabarni bazaga saqlash
-        await self.save_message(username, message)
+  async def receive(self, text_data):
+    data = json.loads(text_data)
+    message = data['message']
+    username = data['username']
 
-        # Xabarni guruhdagi barchaga yuborish
-        await self.channel_layer.group_send(
-            self.room_group_name,
-            {
-                'type': 'chat_message',
-                'message': message,
-                'username': username
-            }
-        )
+    # Xabarni IP manzili bilan birga bazaga saqlash
+    await self.save_message(username, message, self.client_ip)
 
-    async def chat_message(self, event):
-        message = event['message']
-        username = event['username']
+    await self.channel_layer.group_send(
+        self.room_group_name,
+        {
+            'type': 'chat_message',
+            'message': message,
+            'username': username,
+            'ip_address': self.client_ip,  # IP ni ham hammaga yuborish mumkin (yoki faqat o'zingiz ko'rishingiz uchun qoldirish mumkin)
+        },
+    )
 
-        # Brauzerga ma'lumotni yuborish
-        await self.send(
-            text_data=json.dumps({
-                'message': message,
-                'username': username
-            })
-        )
+  async def chat_message(self, event):
+    message = event['message']
+    username = event['username']
 
-    @database_sync_to_async
-    def save_message(self, username, message):
-        Message.objects.create(username=username, body=message)
+    await self.send(
+        text_data=json.dumps({'message': message, 'username': username})
+    )
+
+  @database_sync_to_async
+  def save_message(self, username, message, ip_address):
+    Message.objects.create(
+        username=username, body=message, ip_address=ip_address
+    )
--- messe/models.py
+++ messe/models.py
@@ -2,9 +2,12 @@ from django.db import models
 
 
 class Message(models.Model):
-  username = models.CharField(max_length=100)  # Foydalanuvchi kiritgan ism
-  body = models.TextField()  # Xabar matni
-  created = models.DateTimeField(auto_now_add=True)  # Vaqti
+  username = models.CharField(max_length=100)
+  body = models.TextField()
+  ip_address = models.CharField(
+      max_length=50, blank=True, null=True
+  )  # IP uchun
+  created = models.DateTimeField(auto_now_add=True)
 
   def __str__(self):
     return f"{self.username}: {self.body[:20]}"
